0502777650c164cd37dd526c5d8ae6b21aeb967c
Jedeye <jody.septiawan5@gmail.com>
2022-07-22 11:02:44 +0700

    Day 5

--- blog.js
+++ blog.js
@@ -1,13 +1,3 @@
-// Object
-// Array
-// Looping
-// HTML ELement Manipulation
-
-// 1. Get data form ✅
-// 2. Grouping by Object ✅
-// 3. Store to Array ✅
-// 4. Display to HTML page  ✅
-
 let blogs = [] // Variable Global
 
 function addBlog() {
@@ -61,13 +51,100 @@ function renderBlog() {
                 >
             </h1>
             <div class="detail-blog-content">
-            ${blogs[i].postAt} | ${blogs[i].author}
+            ${getFullTime(blogs[i].postAt)} | ${blogs[i].author}
             </div>
             <p>${blogs[i].content}</p>
+            <div style="text-align: right; color: grey; font-size: 15px">
+                ${getDistanceTime(blogs[i].postAt)}
+            </div>
             </div>
         </div>`
-        
     }
-
 }
 
+// 1. Convert Format Time ✅
+// 2. Count Duration
+
+
+function getFullTime(time) {
+
+    let month = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
+
+    // tanggal => getDate()
+    // bulan => getMonth()
+    // tahun => getFullYear()
+    // jam => getHours()
+    // menit => getMinutes()
+
+    let date = time.getDate()
+    let monthIndex = time.getMonth()
+    let year = time.getFullYear()
+    let hour = time.getHours()
+    let minute = time.getMinutes()
+
+    let result = `${date} ${month[monthIndex]} ${year} ${hour}:${minute} WIB`
+
+    // console.log(time);
+    // console.log(result);
+
+    return result;
+}
+
+// 1. Hari = milisecondInSecond * secondsInMinute * minuteInHour * hoursInDay
+// 2. jam = milisecondInSecond * secondsInMinute * minuteInHour
+// 3. menit = milisecondInSecond * secondsInMinute
+// 4. detik = milisecondInSecond
+
+function getDistanceTime(time) {
+    let blogPostAt = new Date(time); // Waktu Blog di post
+    let currentTime = new Date() // Waktu saat ini
+
+    let distance = currentTime - blogPostAt; // milisecond
+
+    // Convert to Day
+    let dayDistance = Math.floor(distance / (1000 * 60 * 60 * 24))
+
+    if(dayDistance > 0) {
+        return `${dayDistance} day ago`;
+    } else {
+        // Convert to Hour
+        let hourDistance = Math.floor(distance / (1000 * 60 * 60))
+
+        if(hourDistance > 0) {
+            return `${hourDistance} hours ago`;
+        } else {
+            // Convert to Minute
+            let minuteDistance = Math.floor(distance / (1000 * 60))
+
+            if (minuteDistance > 0) {
+                return `${minuteDistance} minute ago`;
+            } else {
+                // Convert to Second
+                let secondDistance = Math.floor(distance / (1000))
+
+                return `${secondDistance} second ago`;
+            }
+        }
+
+    }
+}
+
+// Eksekusi code selama interval (second,minute,etc) yang ditentukan
+setInterval(intervalFunction, 1000)
+
+function intervalFunction() {
+    renderBlog()
+}
